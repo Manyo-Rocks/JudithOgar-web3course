6cfcfee6b89e62ea06d94277bf8a08a5d9d904c0
Manyo-Rocks <judithmanyo2@gmail.com>
2024-11-08 18:07:58 +0100

    module 1 exercises completed

---
+++ module1/program-structure/exercises.js
@@ -0,0 +1,36 @@
+//pyramid 
+let pyramid = 7;
+
+for (let pyramid = "#"; pyramid.length <= 7; pyramid += "#") {
+    console.log(pyramid);
+  }
+
+//fizzbuzz
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+      console.log("Fizz");
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(num);
+    }
+  }
+
+  //chessboard 
+  let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 === 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
